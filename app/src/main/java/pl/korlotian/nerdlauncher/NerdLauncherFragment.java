ac1014e9d3b04cad2d9455aafa9946676e164da3
Maria Korlotian <maria.stoklosa@netguru.pl>
2017-03-15 19:14:20 +0100

    List activities being found

--- app/src/main/java/pl/korlotian/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/pl/korlotian/nerdlauncher/NerdLauncherFragment.java
@@ -1,14 +1,22 @@
 package pl.korlotian.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class NerdLauncherFragment extends Fragment {
+    private static final String TAG = "NerdLauncherFragment";
+
     private RecyclerView mRecyclerView;
 
     public static NerdLauncherFragment newInstance() {
@@ -23,6 +31,17 @@ public class NerdLauncherFragment extends Fragment {
                 .findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        setupAdapter();
         return v;
+    }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
     }
 }
